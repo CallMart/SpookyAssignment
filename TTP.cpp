36d8311946879092ef38ac1084609ae4bf8a186c
CallMart <38089331+CallMart@users.noreply.github.com>
2018-10-30 18:34:54 +1030

    Add files via upload

---
+++ TTP.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+class City{
+private:
+	int cityNo;
+	int locX;
+	int locY;
+public:
+	City(){
+	};
+	City(int N,int X, int Y){
+		cityNo = N;
+		locX = X;
+		locY = Y;
+	};
+	int calculateDistance(int X, int Y){
+		int Distance;
+		Distance = sqrt(pow(locX-X,2)+pow(locY-Y,2));
+		return Distance;
+	}
+	int giveX(){
+		return locX;
+	}
+	int giveY(){
+		return locY;
+	}
+	int giveNo(){
+		return cityNo;
+	}
+};
+
+int closestCity(int X, int Y,vector<City> cities){
+	int cityNumber;
+	int closestDistance = cities[0].calculateDistance(X,Y);
+	for(int i = 0; i < cities.size(); i++){
+		int calculatedDistance = cities[i].calculateDistance(X,Y);
+		if(closestDistance > calculatedDistance && calculatedDistance > 0){
+			closestDistance = calculatedDistance;
+		} else if(closestDistance > calculatedDistance * -1 && calculatedDistance < 0){
+			closestDistance = calculatedDistance;
+		}
+	}
+	for(int i = 0; i < cities.size(); i++){
+		int calculatedDistance = cities[i].calculateDistance(X,Y);
+		if(closestDistance == calculatedDistance){
+			cityNumber = i;
+			break;
+		}
+	}
+	return cityNumber;
+}
+
+int main(){
+	City test1,test2,test3,test4;
+	test1 = City(1,0,0);
+	test2 = City(2,0,10);
+	test3 = City(3,10,0);
+	test4 = City(4,10,10);
+	vector<City> CityHolder;
+	CityHolder.push_back(test1);
+	CityHolder.push_back(test2);
+	CityHolder.push_back(test3);
+	CityHolder.push_back(test4);
+	int currentX = CityHolder[0].giveX();
+	int currentY = CityHolder[0].giveY();
+	vector<City> VisitableCities = CityHolder;
+	vector<City> VisitedCities;
+	VisitedCities.push_back(VisitableCities[0]);
+	VisitableCities.erase(VisitableCities.begin() + 0);
+	while(VisitableCities.size() > 0){
+		int theclosestcity = closestCity(currentX,currentY,VisitableCities);
+		currentX = VisitableCities[theclosestcity].giveX();
+		currentY = VisitableCities[theclosestcity].giveY();
+		VisitedCities.push_back(VisitableCities[theclosestcity]);
+		VisitableCities.erase(VisitableCities.begin() + theclosestcity);
+	}
+	cout << "[";
+	for(int i = 0 ; i < VisitedCities.size(); i++){
+		cout << VisitedCities[i].giveNo() << ",";
+	}
+	cout << "]";
+	return 0;
+}
